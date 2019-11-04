f6b24860264aad0fdc4cd62e987aa6fface733a2
jose14520 <jose14520@gmail.com>
2019-11-04 15:19:47 -0800

    connected login to whatever chat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to '/whatever', notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: '/whatever' }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :users
   resources :whatever_chats
   root 'home#index'
+  get '/users/new', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/whatever', to: 'whatever_chats#index'
+  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
