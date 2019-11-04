56f4431d9e525a31f34a60109d2b5dbd7815c63e
jose14520 <jose14520@gmail.com>
2019-11-04 14:39:09 -0800

    got a login screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   resources :whatever_chats
+  root 'home#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_053813) do
+ActiveRecord::Schema.define(version: 2019_11_04_221443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
   create_table "whatever_chats", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
