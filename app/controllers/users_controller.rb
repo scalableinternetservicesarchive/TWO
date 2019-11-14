9daa9940a8d305fe1f6d519b0eed3b34c31d2dbf
jose14520 <jose14520@gmail.com>
2019-11-13 22:26:22 -0800

    Signup starts new session. Unauth home button fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to '/whatever', notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: '/whatever' }
       else
