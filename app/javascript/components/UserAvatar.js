b040a1213db6f6b3d9c1d8e1390b2b8121ab4645
mdy <zbshfmmm@gmail.com>
2019-11-16 17:48:45 -0800

    link to from/person

--- app/javascript/components/UserAvatar.js
+++ app/javascript/components/UserAvatar.js
@@ -4,10 +4,11 @@ import PropTypes from "prop-types";
 class UserAvatar extends React.Component {
   render() {
     return (
-      <>
-      <img src={require(`../images/avatars/${this.props.username}.png`)}
-           height={18} width={18} alt={"avatar"} />{this.props.username}
-      </>
+      <a href={`/from/${this.props.username}`}>
+        <img src={require(`../images/avatars/${this.props.username}.png`)}
+             height={18} width={18} alt={"avatar"} />
+             {this.props.username}
+      </a>
     )
   }
 }
