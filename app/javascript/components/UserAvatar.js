f00a366bf6964895e6a6cf1e10e911d4e67d5ec2
mdy <zbshfmmm@gmail.com>
2019-11-16 18:13:11 -0800

    fix default, add link to user

--- app/javascript/components/UserAvatar.js
+++ app/javascript/components/UserAvatar.js
@@ -3,9 +3,20 @@ import PropTypes from "prop-types";
 
 class UserAvatar extends React.Component {
   render() {
+    let ava;
+    try {
+      ava = require(`../images/avatars/${this.props.username}.png`);
+    }
+    catch (e) {
+      if (e.code !== 'MODULE_NOT_FOUND') {
+        throw e;
+      }
+      ava = require(`../images/avatars/__default.png`);
+    }
+
     return (
       <a href={`/from/${this.props.username}`}>
-        <img src={require(`../images/avatars/${this.props.username}.png`)}
+        <img src={ava}
              height={18} width={18} alt={"avatar"} />
              {this.props.username}
       </a>
