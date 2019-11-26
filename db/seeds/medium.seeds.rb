9ee66187e44e7acb3fe81d4e5a8dde1e057703e1
Karl Wang <changhai_wang@ucsb.edu>
2019-11-25 22:01:17 -0800

    avoid name conflict

--- db/seeds/medium.seeds.rb
+++ db/seeds/medium.seeds.rb
@@ -2,7 +2,11 @@ require 'pp'
 
 names = File.readlines(Rails.root.join('db','seeds','medium.csv')).map{ |line| line.chop }
 
-names.each { |name| User.create!(username: name, password: 'password') }
+names.each { |name| 
+  if !User.where(username: name).present?
+    User.create!(username: name, password: 'password')
+  end
+}
 1000.times { WhateverChat.create!(title: 'Meetup at San' + names.sample, 
                                   body: 'Hey guys, we are meeting at San Clemente Center 5pm today for the future of the universe. Snacks and soft drinks available!', 
                                   to_user_id: '0', 
